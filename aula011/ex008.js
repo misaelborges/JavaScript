95f7d9e5a6202d4a81c85cbb19cc1fa707a68685
misael borges cancelier <mizaelborges2011@gmail.com>
2022-08-02 11:29:54 -0300

    exercicios com javascript

---
+++ aula011/ex008.js
@@ -0,0 +1,7 @@
+var vel = 90
+console.log(`Voce esta dirigindo a ${vel}Km/h`)
+if (vel > 70) {
+    console.log('Voce esta acima da velocidade permetida. MULTADO!!!')
+} else {
+    console.log('Voce esta dentro da velocidade permitida')
+}
