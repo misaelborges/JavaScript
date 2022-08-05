4adcb9c0abf409f6ae3b6bf7e3ac6d0c91eb97ec
misael borges cancelier <mizaelborges2011@gmail.com>
2022-08-05 19:02:26 -0300

    verificador de idade

---
+++ verificardorDeIdade/javascript/script.js
@@ -0,0 +1,20 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value == 0 || fano.value > ano) {
+        window.alert('[ERRO] verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        if (fsex[0].checked) {
+            genero = 'Homem'
+        } else {
+            genero = 'Mulher'
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    }
+
+}
