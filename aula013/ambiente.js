369c398baa47288f5939556300424835e7e2d2ac
misael borges cancelier <mizaelborges2011@gmail.com>
2022-08-05 12:16:41 -0300

    Create ambiente.js

---
+++ aula013/ambiente.js
@@ -0,0 +1,15 @@
+var pizza = 1
+while(pizza <= 2) {
+    console.log(`Comendo a fatia n° ${pizza}`)
+    pizza++
+}
+console.log('acabou as fatias seu guloso')
+
+console.log('---------------------------')
+
+var pizza = 1
+do {
+    console.log(`Comendo a fatia n° ${pizza}`)
+    pizza++
+} while(pizza <= 2)
+console.log('acabou as fatias seu guloso')
