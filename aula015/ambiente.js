ad6a91aa1e1b997880fc77b732a4d63c9fb6ee13
misael borges cancelier <mizaelborges2011@gmail.com>
2022-09-10 15:51:15 -0300

    estudos de JavaScript

---
+++ aula015/ambiente.js
@@ -0,0 +1,5 @@
+let num = ['g' , 'b' , 'a']
+num.sort()
+for (let pos in num) {
+    console.log(`A posição ${pos} tem o valor: ${num[pos]}`)
+}
---
+++ aula016/funcao01.js
@@ -0,0 +1,10 @@
+function parImpar(n) {
+    if (n%2==0) {
+        return 'Par'
+    } else {
+        return 'Impar'
+    }
+}
+
+let num = parImpar(2)
+console.log(num)
